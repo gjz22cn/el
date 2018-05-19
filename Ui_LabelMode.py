7ac965eb0ecb1e4761531fa49b2f06859df98a3d
james <gjz22cn@hotmail.com>
2018-05-19 18:41:35 +0800

    update for labelmode.ui

--- Ui_LabelMode.py
+++ Ui_LabelMode.py
@@ -7,34 +7,52 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QFileDialog
 
 class Ui_LabelModeWindow(object):
     def setupUi(self, LabelModeWindow):
         LabelModeWindow.setObjectName("LabelModeWindow")
-        LabelModeWindow.resize(800, 522)
+        LabelModeWindow.resize(953, 768)
         self.centralWidget = QtWidgets.QWidget(LabelModeWindow)
         self.centralWidget.setObjectName("centralWidget")
-        self.graphicsView = QtWidgets.QGraphicsView(self.centralWidget)
-        self.graphicsView.setGeometry(QtCore.QRect(50, 70, 551, 361))
-        self.graphicsView.setObjectName("graphicsView")
-        
-        self.graphicsView.scene = QtWidgets.QGraphicsScene()
-        item=QtWidgets.QGraphicsPixmapItem(p)
-        self.graphicsView.scene.addItem(item)
-        self.graphicsView.setScene(self.graphicsView.scene)
-        
-        self.gridLayoutWidget = QtWidgets.QWidget(self.centralWidget)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(620, 340, 160, 80))
-        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
-        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
-        self.gridLayout.setContentsMargins(0, 0, 0, 0)
-        self.gridLayout.setObjectName("gridLayout")
-        self.pushButton = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralWidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(850, 500, 160, 41))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton = QtWidgets.QPushButton(self.horizontalLayoutWidget)
         self.pushButton.setObjectName("pushButton")
-        self.gridLayout.addWidget(self.pushButton, 0, 0, 1, 1)
-        self.pushButton_2 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.horizontalLayout.addWidget(self.pushButton)
+        self.pushButton_2 = QtWidgets.QPushButton(self.horizontalLayoutWidget)
         self.pushButton_2.setObjectName("pushButton_2")
-        self.gridLayout.addWidget(self.pushButton_2, 1, 0, 1, 1)
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralWidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(850, 70, 160, 122))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.label.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.listView = QtWidgets.QListView(self.verticalLayoutWidget)
+        self.listView.setObjectName("listView")
+        self.verticalLayout.addWidget(self.listView)
+        self.pushButton_3 = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.verticalLayout.addWidget(self.pushButton_3)
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralWidget)
+        self.pushButton_4.setGeometry(QtCore.QRect(860, 20, 75, 23))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton.clicked.connect(self.openPicDir)
+        
+        self.widget = QtWidgets.QWidget(self.centralWidget)
+        self.widget.setGeometry(QtCore.QRect(39, 29, 801, 511))
+        self.widget.setObjectName("widget")
+        self.widget.setStyleSheet("border-image:url(F:/code/python/el/sample.jpg)");
         LabelModeWindow.setCentralWidget(self.centralWidget)
 
         self.retranslateUi(LabelModeWindow)
@@ -43,30 +61,23 @@ class Ui_LabelModeWindow(object):
     def retranslateUi(self, LabelModeWindow):
         _translate = QtCore.QCoreApplication.translate
         LabelModeWindow.setWindowTitle(_translate("LabelModeWindow", "MainWindow"))
-        self.pushButton.setText(_translate("LabelModeWindow", "PushButton"))
-        self.pushButton_2.setText(_translate("LabelModeWindow", "PushButton"))
+        self.pushButton.setText(_translate("LabelModeWindow", "下一张"))
+        self.pushButton_2.setText(_translate("LabelModeWindow", "上一张"))
+        self.label.setText(_translate("LabelModeWindow", "标签列表"))
+        self.pushButton_3.setText(_translate("LabelModeWindow", "确认"))
+        self.pushButton_4.setText(_translate("LabelModeWindow", "打开文件夹"))
+    
+    def openPicDir(self):
+         self.picDir = QFileDialog.getExistingDirectory(self, "Open a folder", "E:/")
+
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     LabelModeWindow = QtWidgets.QMainWindow()
-
-    
-    image = QtGui.QImage('F:/opencv2/NG/100616.jpg')
-    #w = ui.graphicsView.geometry.Width
-    #h = ui.graphicsView.geometry.Height
-    qimage = image.scaled(551, 361, QtCore.Qt.KeepAspectRatioByExpanding)
-    p = QtGui.QPixmap.fromImage(qimage)
-    
     ui = Ui_LabelModeWindow()
     ui.setupUi(LabelModeWindow)
-    '''
-    p=QtGui.QPixmap(60, 60)
-    p.load('F:/opencv2/NG/100616.jpg') 
-    '''
-    
-    
     LabelModeWindow.show()
     sys.exit(app.exec_())
 
