f2fd7b5c1166330eccebda3a66f1525b85895ae0
james <gjz22cn@hotmail.com>
2018-05-21 23:14:55 +0800

    update

--- Ui_LabelMode.py
+++ Ui_LabelMode.py
@@ -5,18 +5,29 @@
 # Created by: PyQt5 UI code generator 5.10.1
 #
 # WARNING! All changes made in this file will be lost!
-
-from PyQt5 import QtCore, QtGui, QtWidgets
+import os
+from PyQt5 import QtCore, QtWidgets
 from Ui_MainWidget import Ui_MainWidget
+from PyQt5.QtWidgets import QFileDialog, QWidget
 
-class Ui_LabelModeWindow(object):
+class Ui_LabelModeWindow(QWidget):
     def setupUi(self, LabelModeWindow):
+        g_width,  g_height= 4896,  3034
+        g_top_margin,  g_right_margin,  g_buttom_margin,  g_left_margin = 48,  48,  104,  48
+        w = 8
+        self.width = g_width/w
+        self.height = g_height/w
+        self.top_margin = g_top_margin/w
+        self.right_margin = g_right_margin/w
+        self.buttom_margin = g_buttom_margin/w
+        self.left_margin = g_left_margin/w
+        self.piece_size = 480/w
         LabelModeWindow.setObjectName("LabelModeWindow")
-        LabelModeWindow.resize(1433, 785)
+        LabelModeWindow.resize(self.width+200, self.height+50)
         self.centralWidget = QtWidgets.QWidget(LabelModeWindow)
         self.centralWidget.setObjectName("centralWidget")
         self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralWidget)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(1250, 710, 160, 41))
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(self.width+10, 50, 160, 41))
         self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
@@ -28,22 +39,22 @@ class Ui_LabelModeWindow(object):
         self.pushButton_2.setObjectName("pushButton_2")
         self.horizontalLayout.addWidget(self.pushButton_2)
         self.pushButton_4 = QtWidgets.QPushButton(self.centralWidget)
-        self.pushButton_4.setGeometry(QtCore.QRect(1250, 670, 75, 23))
+        self.pushButton_4.setGeometry(QtCore.QRect(self.width+10, 10, 75, 23))
         self.pushButton_4.setObjectName("pushButton_4")
         
         #self.tableWidget = QtWidgets.QTableWidget(self.centralWidget)
         self.tableWidget = Ui_MainWidget(self.centralWidget)
-        self.tableWidget.setGeometry(QtCore.QRect(10, 10, 1224, 758))
+        self.tableWidget.setGeometry(QtCore.QRect(10, 10, self.width, self.height))
         
+        self.pushButton_4.clicked.connect(self.openPicDir)
+        self.pushButton.clicked.connect(self.nextPic)
+        self.pushButton_2.clicked.connect(self.prePic)
         
-        self.horizontalLayoutWidget.raise_()
-        self.pushButton_4.raise_()
-        self.tableWidget.raise_()
-        self.pushButton_2.raise_()
         LabelModeWindow.setCentralWidget(self.centralWidget)
 
         self.retranslateUi(LabelModeWindow)
         QtCore.QMetaObject.connectSlotsByName(LabelModeWindow)
+        self.picDir = ''
 
     def retranslateUi(self, LabelModeWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -51,7 +62,42 @@ class Ui_LabelModeWindow(object):
         self.pushButton.setText(_translate("LabelModeWindow", "下一张"))
         self.pushButton_2.setText(_translate("LabelModeWindow", "上一张"))
         self.pushButton_4.setText(_translate("LabelModeWindow", "打开文件夹"))
-
+    
+    def openPicDir(self):
+         self.picDir = QFileDialog.getExistingDirectory(self, "Open a folder", "F:/")
+         print(self.picDir)
+         self.fileList = os.listdir(self.picDir)
+         self.fileCnt = len(self.fileList)
+         self.fileIndex = 0
+                
+    def nextPic(self):
+        if self.picDir == '':
+            return
+        self.fileIndex = self.fileIndex + 1
+        if self.fileIndex == self.fileCnt:
+            self.fileIndex = self.fileCnt -1
+        self.loadPic()
+        
+    def prePic(self):
+        if self.picDir == '':
+            return
+        self.fileIndex = self.fileIndex - 1
+        if self.fileIndex == -1:
+            self.fileIndex = 0
+        self.loadPic()
+        
+    def loadPic(self): 
+        self.filePath = self.picDir+'/'+self.fileList[self.fileIndex]
+        filepath,shortname,extension = self.getFilePathNameExt(self.filePath)
+        if not extension == ".jpg":
+            return
+        self.tableWidget.setStyleSheet("QTableWidget {padding:%dpx %dpx %dpx %dpx; border-image:url(%s)}"%(self.top_margin, self.right_margin, self.buttom_margin, self.left_margin, self.filePath))
+    
+    def getFilePathNameExt(self, filename):  
+        (filepath,tempfilename) = os.path.split(filename);  
+        (shotname,extension) = os.path.splitext(tempfilename);  
+        return filepath,shotname,extension
+    
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
--- Ui_MainWidget.py
+++ Ui_MainWidget.py
@@ -2,53 +2,34 @@ from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel, QTableWidget
 from PyQt5 import QtCore, QtGui, QtWidgets
 from Ui_LabelType import Ui_LabelTypeDialog
 
-'''
-class Ui_MainWidget(QWidget):
-    def __init__(self, parent=None):
-        super(Ui_MainWidget,self).__init__(parent)
-        #self.setObjectName("myQWidget")
-        self.setGeometry(QtCore.QRect(0, 0, 801, 511))
-        
-        self.setAutoFillBackground(True)
-        palette = QtGui.QPalette()
-        pixmap = QtGui.QPixmap('F:/code/python/el/sample.jpg').scaled(self.size())
-        palette.setBrush(self.backgroundRole(), QtGui.QBrush(pixmap)) 
-        self.setPalette(palette)
-        
-        
-        
-        #self.setStyleSheet("#myQWidget {background-image:url(F:/code/python/el/sample.jpg)}");
-        
-        self.grid = QGridLayout()
-        self.grid.setSpacing(0)
-        self.setLayout(self.grid) 
-        pos = [(i, j) for i in range(6) for j in range(10)]
-        for i in range(len(pos)):
-            self.grid.addWidget(QLabel('haha'), pos[i][0], pos[i][1])
-    
-    def mouseDoubleClickEvent(self, event):
-        print("double1 Click")
-        location = self.grid.getItemPosition(idx)
-        print("at row/col", location[:2])
-'''
-
 class Ui_MainWidget(QTableWidget):
     def __init__(self, parent=None):
         super(Ui_MainWidget,self).__init__(parent)
         #self.setObjectName("myQWidget")
         g_width,  g_height= 4896,  3034
         g_top_margin,  g_right_margin,  g_buttom_margin,  g_left_margin = 48,  48,  104,  48
-        self.setGeometry(QtCore.QRect(0, 0, 1224, 758))
+        w = 8
+        self.width = g_width/w
+        self.height = g_height/w
+        self.top_margin = g_top_margin/w
+        self.right_margin = g_right_margin/w
+        self.buttom_margin = g_buttom_margin/w
+        self.left_margin = g_left_margin/w
+        self.piece_size = 480/w
+        
+        
+        
+        self.setGeometry(QtCore.QRect(0, 0, self.width, self.height))
         
         self.setColumnCount(10)
         self.setRowCount(6)
         
         self.setVerticalHeaderLabels(['A','B','C', 'D', 'E', 'F'])
         self.horizontalHeader().setVisible(False)
-        self.horizontalHeader().setDefaultSectionSize(120)
+        self.horizontalHeader().setDefaultSectionSize(self.piece_size)
         self.verticalHeader().setVisible(False)
-        self.verticalHeader().setDefaultSectionSize(120)
-        self.setStyleSheet("QTableWidget {padding:12px 12px 26px 12px; border-image:url(F:/code/python/el/sample.jpg)}");
+        self.verticalHeader().setDefaultSectionSize(self.piece_size)
+        self.setStyleSheet("QTableWidget {padding:%dpx %dpx %dpx %dpx; border-image:url(F:/code/python/el/sample.jpg)}"%(self.top_margin, self.right_margin, self.buttom_margin, self.left_margin));
         self.setEditTriggers(QTableWidget.NoEditTriggers)
         self.cellClicked.connect(self.popLabelDialog)
 
